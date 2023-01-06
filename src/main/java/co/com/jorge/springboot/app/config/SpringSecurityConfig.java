54d3be9edc5f778a7824b8dce47c38553a53f352
jorgevelasquezv <jorgeluisvelasquezv@gmail.com>
2023-01-05 20:27:09 -0500

    Se implementa seguridad con spring security con JDBC

--- src/main/java/co/com/jorge/springboot/app/config/SpringSecurityConfig.java
+++ src/main/java/co/com/jorge/springboot/app/config/SpringSecurityConfig.java
@@ -13,6 +13,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+import javax.sql.DataSource;
+
 import static org.springframework.security.config.Customizer.withDefaults;
 
 // https://docs.spring.io/spring-security/reference/servlet/authorization/authorize-http-requests.html
@@ -23,6 +25,9 @@ public class SpringSecurityConfig{
 
     @Autowired
     private LoginSuccessHandler loginSuccessHandler;
+
+    @Autowired
+    private DataSource dataSource;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -59,11 +64,18 @@ public class SpringSecurityConfig{
 
     @Autowired
     public void configurerGlobal(AuthenticationManagerBuilder builder) throws Exception {
-        PasswordEncoder encoder = passwordEncoder();
-        UserBuilder users = User.builder().passwordEncoder(encoder::encode);
-        builder.inMemoryAuthentication()
-                .withUser(users.username("admin").password("12345").roles("ADMIN", "USER"))
-                .withUser(users.username("jorge").password("12345").roles("USER"));
+//        PasswordEncoder encoder = passwordEncoder();
+//        UserBuilder users = User.builder().passwordEncoder(encoder::encode);
+//        builder.inMemoryAuthentication()
+//                .withUser(users.username("admin").password("12345").roles("ADMIN", "USER"))
+//                .withUser(users.username("jorge").password("12345").roles("USER"));
+        builder.jdbcAuthentication()
+                .dataSource(dataSource)
+                .passwordEncoder(passwordEncoder())
+                .usersByUsernameQuery("select username, password, enabled from users where username=?")
+                .authoritiesByUsernameQuery("select u.username, a.authority from authorities a inner join users u on (a.user_id=u.id) where u.username=?");
+
+
     }
 
 }
